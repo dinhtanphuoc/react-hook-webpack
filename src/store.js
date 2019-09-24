77a5ba269b658a414a6d6e3ea0ecbcf5d4cb5f2b
PhuocDT <phuocdt@greenglobal.vn>
2019-09-24 15:17:04 +0700

    setup redux sagas

--- src/store.js
+++ src/store.js
@@ -1,18 +1,21 @@
 import { applyMiddleware, compose, createStore } from 'redux';
 import createSagaMiddleware from 'redux-saga';
-import rootReducer from './appReducers';
-import usersSaga from "../redux-saga/sagas";
+import rootReducer from './reducers';
+import rootSaga from './sagas';
 
 const sagaMiddleware = createSagaMiddleware();
 
-export function configureStore(initialState) { 
+const configureStore = initialState => {
   const middleware = [sagaMiddleware];
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; 
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   const store = createStore(
-    rootReducer, 
-    initialState, 
+    rootReducer,
+    initialState,
     composeEnhancers(applyMiddleware(...middleware))
   );
-  sagaMiddleware.run(usersSaga);
+  sagaMiddleware.run(rootSaga);
   return store;
 };
+
+export default configureStore;
--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,9 @@ module.exports = {
     path: path.join(__dirname, "build"),
     filename: "bundle.js"
   },
+  resolve: {
+    extensions: [ '.js', '.jsx' ]
+  },
   module: {
     rules: [
       {
